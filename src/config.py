d29c5f99bdca36b56d27114d8e0222f094cce718
bhashyam.anirudh <bhashyam.anirudh@gmail.com>
2022-09-07 11:57:15 +0530

    Minor changes to conf

--- src/config.py
+++ src/config.py
@@ -6,23 +6,14 @@ from dataclasses import dataclass
 
 @dataclass
 class Config:
-    """
-    General configuration parser class.
-    """
     config_dir: Path
     filenames: list[str]
 
     def _read_file(self, filename: str):
-        """
-        Read a single configuration file and yield a dictionary.
-        """
         with self.config_dir.joinpath(filename).open("r") as f:
            yield json.load(f)
     
     def load(self) -> dict[str, str]:
-        """
-        Read all files and yield dictionaries.
-        """
         for file in self.filenames:
             yield from self._read_file(file)
 
